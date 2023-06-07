0ccf8a61394a7eb5de6c1961d1cb7cccf899c3c1
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-07 21:48:19 +0200

    create single-project page

---
+++ wp-content/themes/portfolio/single-projet.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+	<main class="project">
+		<h1 class="project__title"><?= get_the_title(); ?></h1>
+		<p> <?= get_field('project_description') ?> </p>
+		<a href="<?= get_field('project_link') ?>"> Visiter le projet </a>
+	</main>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
