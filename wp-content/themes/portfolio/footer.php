15eb4ba4899cf5d6e9b818d4b2213359aa20107e
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-07 12:55:02 +0200

    adding address in footer

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,13 +1,13 @@
 <footer class="footer">
 	<ul class="footer__links">
+	<p class="footer__copyright">© <?=get_bloginfo('name');?> - <time datetime="2023">2023</time> </p>
+	<p>4000 Liège - Belgique </p>
 		<?php foreach(portfolio_get_menu('footer') as $link): ?>
 			<li class="footer__link">
 				<a href="<?= $link->href; ?>" class="footer__url"><?= $link->label; ?></a>
 			</li>
 		<?php endforeach; ?>
 	</ul>
-	<p class="footer__copyright">© <?=get_bloginfo('name');?> - <time datetime="2023">2023</time> </p>
-	<p> Belgique - 4000 Liège </p>
 </footer>
 </body>
 </html>
