b1901239bdee3b0f8e10327f7b66c4d18f69e5b1
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-07 21:57:02 +0200

    update template-accueil.php

--- wp-content/themes/portfolio/template-accueil.php
+++ wp-content/themes/portfolio/template-accueil.php
@@ -1,53 +1,52 @@
 <?php /* Template Name: Landing page template */ ?>
 <?php get_header(); ?>
-<?php if (have_posts()): while (have_posts()):
-the_post(); ?>
-<main>
-	<div class="hero">
-		<h1 class="header__sitename"><?= get_bloginfo('name'); ?></h1>
-		<p class="header__tagline"><?= get_bloginfo('description'); ?></p>
-	</div>
-	<section class="description">
-		<h2><?= get_field('description_title') ?></h2>
-		<?php foreach (get_field('qualities') as $quality): ?>
-			<p> <?= $quality['name'] ?> </p>
-		<?php endforeach; ?>
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+	<main>
+		<div class="hero">
+			<h1 class="header__sitename"><?= get_bloginfo('name'); ?></h1>
+			<p class="header__tagline"><?= get_bloginfo('description'); ?></p>
+		</div>
+		<section class="description">
+			<h2><?= get_field('description_title') ?></h2>
+			<?php foreach (get_field('qualities') as $quality): ?>
+				<p> <?= $quality['name'] ?> </p>
+			<?php endforeach; ?>
 
-		<?php foreach (get_field('description') as $description): ?>
-			<p> <?= $description['paragraphe'] ?> </p>
-		<?php endforeach; ?>
+			<?php foreach (get_field('description') as $description): ?>
+				<p> <?= $description['paragraphe'] ?> </p>
+			<?php endforeach; ?>
 
-		<?php foreach(get_field('legends_picture') as $legend): ?>
-			<p> <?= $legend['text']; ?> </p>
-		<?php endforeach; ?>
-	</section>
+			<?php foreach(get_field('legends_picture') as $legend): ?>
+				<p> <?= $legend['text']; ?> </p>
+			<?php endforeach; ?>
+		</section>
 
-	<div class="project_container">
-		<?php
+		<div class="project_container">
+			<?php
+			$call_to_action_text = get_field('call_to_action');
 
-		// Faire une requête en DB pour récupérer 3 projets
-		$projects = new WP_Query([
-				'post_type' => 'projet',
-				'posts_per_page' => 3
-		]);
+			// Faire une requête en DB pour récupérer 3 projets
+			$projects = new WP_Query([
+					'post_type' => 'projet',
+					'posts_per_page' => 2
+			]); ?>
 
-		// Lancer la boucle pour afficher chaque projet
-		if ($projects->have_posts()): while ($projects->have_posts()):
-		$projects->the_post();
-		?>
-		<article>
+			<?php // Lancer la boucle pour afficher chaque projet
+			if ($projects->have_posts()): while ($projects->have_posts()):
+				$projects->the_post();
+				?>
+				<article>
+					<a href="<?= get_the_permalink(); ?>" class="project__link">
+						<span class=""><?= get_the_title(); ?></span>
+					</a>
+				</article>
+			<?php endwhile; else: ?>
+				<p>Je n'ai aucun projet pour l'instant.</p>
+			<?php endif; ?>
+		</div>
+<?php wp_reset_query(); ?>
+		<a href="">  <?= get_field('call_to_action') ?> </a>
+	</main>
+<?php endwhile; endif; ?>
 
-				<a href="<?= get_the_permalink(); ?>" class="project__link">
-					<span class=""><?= get_the_title(); ?></span>
-				</a>
-		</article>
-	</div>
-
-	<?php endwhile; else: ?>
-	<p> j'ai aucun projet pour l'instant </p>
-	<?php endif; ?>
-
-</main>
-<?php endwhile;
-endif; ?>
 <?php get_footer(); ?>
