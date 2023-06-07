efc732cc9afcbeb66aa27bc40d9cb2c5ec1689c8
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-07 12:30:41 +0200

    updating footer and adding content in landpage

---
+++ wp-content/themes/portfolio/template-mentions-legales.php
@@ -0,0 +1,15 @@
+<?php /* Template Name: Landing page template */ ?>
+<?php get_header(); ?>
+<main>
+	<div class="hero">
+		<h1 class="header__sitename"><?= get_bloginfo('name'); ?></h1>
+		<p class="header__tagline"><?= get_bloginfo('description'); ?></p>
+	</div>
+	<section class="description">
+		<h2><?= get_field('description_title')?></h2>
+		<?php foreach(get_field('qualities') as $quality ): ?>
+		<p> <?= $quality['name'] ?> </p>
+		<?php endforeach; ?>
+	</section>
+</main>
+<?php get_footer(); ?>
