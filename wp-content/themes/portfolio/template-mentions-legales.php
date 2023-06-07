bb2d813780e59daaab87c33a5a9840c33049b0d4
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-07 22:00:01 +0200

    update template-mentions-legales.php

--- wp-content/themes/portfolio/template-mentions-legales.php
+++ wp-content/themes/portfolio/template-mentions-legales.php
@@ -1,15 +1,34 @@
 <?php /* Template Name: Privacy policy page template */ ?>
 <?php get_header(); ?>
-<main>
-	<div class="hero">
-		<h1 class="header__sitename"><?= get_bloginfo('name'); ?></h1>
-		<p class="header__tagline"><?= get_bloginfo('description'); ?></p>
-	</div>
-	<section class="description">
-		<h2><?= get_field('description_title')?></h2>
-		<?php foreach(get_field('qualities') as $quality ): ?>
-		<p> <?= $quality['name'] ?> </p>
-		<?php endforeach; ?>
+<main class="legal-mention">
+	<h1 class="legal-mention__title "> <?= get_the_title(); ?> </h1>
+	<article class="legal-mention__general-information">
+		<h2>Informations générales</h2>
+		<dl>
+			<dt>Nom</dt>
+			<dd><?= get_field('surname'); ?></dd>
+			<dt>Prénom</dt>
+			<dd><?= get_field('firstname'); ?></dd>
+			<dt>Numéro de téléphone</dt>
+			<dd><?= get_field('phone_number'); ?></dd>
+			<dt>Rue</dt>
+			<dd><?= get_field('street'); ?></dd>
+			<dt>Numéro de rue</dt>
+			<dd><?= get_field('street_number'); ?></dd>
+			<dt>E-mail</dt>
+			<dd><a href="<?= get_field('email'); ?>"><?= get_field('email'); ?></a></dd>
+			<dt>Ville</dt>
+			<dd><?= get_field('city'); ?></dd>
+			<dt>Pays</dt>
+			<dd><?= get_field('country'); ?></dd>
+		</dl>
+	</article>
+
+	<section class="legal-mention__privacy-policy">
+		<h2>Clause de confidentialité</h2>
+		<p> <?= get_field('privacy_policy')?> </p>
 	</section>
+
+
 </main>
 <?php get_footer(); ?>
