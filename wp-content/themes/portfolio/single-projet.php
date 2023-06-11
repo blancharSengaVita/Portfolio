f16a56a0c06a33eba129e1d65666e1b945f2a24c
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-12 00:23:21 +0200

    add project image

--- wp-content/themes/portfolio/single-projet.php
+++ wp-content/themes/portfolio/single-projet.php
@@ -4,6 +4,9 @@
 		<h1 class="project__title"><?= get_the_title(); ?></h1>
 		<p> <?= get_field('project_description') ?> </p>
 		<a href="<?= get_field('project_link') ?>"> Visiter le projet </a>
+		<figure class="project__fig">
+			<?= get_the_post_thumbnail(null, 'project_medium', ['class' => 'project__img']); ?>
+		</figure>
 	</main>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
