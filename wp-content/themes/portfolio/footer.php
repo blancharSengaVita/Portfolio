2f71385d8d0375fa4f719beb22299f32b071cfaa
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-17 11:37:38 +0200

    rearrange order of footer item

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
-	<ul class="footer__links">
 	<p class="footer__copyright">© <?=get_bloginfo('name');?> - <time datetime="2023">2023</time> </p>
 	<p>4000 Liège - Belgique </p>
+	<ul class="footer__links">
 		<?php foreach(portfolio_get_menu('footer') as $link): ?>
 			<li class="footer__link">
 				<a href="<?= $link->href; ?>" class="footer__url"><?= $link->label; ?></a>
