7836cd5100dade6f1237c037dfcc2eb007aff93e
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-17 11:56:23 +0200

    update contact page

--- wp-content/themes/portfolio/template-contact.php
+++ wp-content/themes/portfolio/template-contact.php
@@ -7,47 +7,51 @@
 		$errors = portfolio_session_get('portfolio_contact_form_errors') ?? [];
 		?>
 
+		<h1 class="title" > <?= get_field('title') ?> </h1>
 
-		<?php if ($feedback): ?>
-			<div class="success" style="border:green;padding:1em;color: green;">
+<!--		--><?php //if ($feedback): ?>
+			<div class="success"">
 				<p>Merci&nbsp;! Votre message a bien été envoyé.</p>
 			</div>
-		<?php else: ?>
+<!--		--><?php //endif; ?>
 
 			<?php if ($errors): ?>
-				<div class="error" style="border:red;padding:1em;color: red;">
+				<div class="error">
 					<p>Attention&nbsp;! Merci de corriger les erreurs du formulaire.</p>
 				</div>
 			<?php endif; ?>
 
 			<form action="<?= esc_url(admin_url('admin-post.php')); ?>" method="POST" class="contact">
 				<fieldset class="contact__info">
-					<div class="field">
-						<label for="firstname" class="field__label">Votre prénom</label>
-						<input type="text" name="firstname" id="firstname" class="field__input"/>
-						<?php if ($errors['firstname'] ?? null): ?>
-							<p class="field__error" style="color:red"><?= $errors['firstname']; ?></p>
-						<?php endif; ?>
+					<div class="contact__container ">
+						<div class="field">
+							<label for="firstname" class="field__label">Votre prénom</label>
+							<input type="text" name="firstname" id="firstname" class="field__input"/>
+							<?php if ($errors['firstname'] ?? null): ?>
+								<p class="field__error" ><?= $errors['firstname']; ?></p>
+							<?php endif; ?>
+						</div>
+						<div class="field">
+							<label for="lastname" class="field__label">Votre nom</label>
+							<input type="text" name="lastname" id="lastname" class="field__input"/>
+							<?php if ($errors['lastname'] ?? null): ?>
+								<p class="field__error" ><?= $errors['lastname']; ?></p>
+							<?php endif; ?>
+						</div>
 					</div>
-					<div class="field">
-						<label for="lastname" class="field__label">Votre nom</label>
-						<input type="text" name="lastname" id="lastname" class="field__input"/>
-						<?php if ($errors['lastname'] ?? null): ?>
-							<p class="field__error" style="color:red"><?= $errors['lastname']; ?></p>
-						<?php endif; ?>
-					</div>
+
 					<div class="field">
 						<label for="email" class="field__label">Votre adresse mail</label>
 						<input type="email" name="email" id="email" class="field__input"/>
 						<?php if ($errors['email'] ?? null): ?>
-							<p class="field__error" style="color:red"><?= $errors['email']; ?></p>
+							<p class="field__error" ><?= $errors['email']; ?></p>
 						<?php endif; ?>
 					</div>
 					<div class="field">
 						<label for="message" class="field__label">Votre message</label>
 						<textarea name="message" id="message" cols="30" rows="10" class="field__textarea"></textarea>
 						<?php if ($errors['message'] ?? null): ?>
-							<p class="field__error" style="color:red"><?= $errors['message']; ?></p>
+							<p class="field__error" ><?= $errors['message']; ?></p>
 						<?php endif; ?>
 					</div>
 				</fieldset>
@@ -57,6 +61,5 @@
 					<button class="contact__submit" type="submit">Envoyer</button>
 				</div>
 			</form>
-		<?php endif; ?>
-	</div>
 </main>
+<?php get_footer(); ?>
