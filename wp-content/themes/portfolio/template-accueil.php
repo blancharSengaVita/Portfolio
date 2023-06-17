7c05ce78f782ad034e3f6f2ddb27eddc004d4090
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-17 11:52:28 +0200

    make the html of homepage

--- wp-content/themes/portfolio/template-accueil.php
+++ wp-content/themes/portfolio/template-accueil.php
@@ -1,86 +1,97 @@
 <?php /* Template Name: Landing page template */ ?>
 <?php get_header(); ?>
-<?php if (have_posts()): while (have_posts()): the_post(); ?>
-	<main>
+<?php if (have_posts()): while (have_posts()):
+	the_post(); ?>
+
+	<main class="main-page">
+
+		<div>
+
+		</div>
+
 		<div class="hero">
-			<h1 class="header__sitename"><?= get_bloginfo('name'); ?></h1>
-			<p class="header__tagline"><?= get_bloginfo('description'); ?></p>
-			<img src="https://portfolio.localhost/wp-content/uploads/2023/06/Logo.png" alt="">
+			<div class="hero__container">
+				<h1 class="hero__title"><?= get_bloginfo('name'); ?></h1>
+				<p class="hero__tagline"><?= get_bloginfo('description'); ?></p>
+			</div>
+			<div class="hero__container">
+				<?= image('https://portfolio.localhost/wp-content/uploads/2023/06/Logo.png', 'hero__img', 'Logo de Blanchar Senga-Vita'); ?>
+			</div>
 		</div>
-		<section class="description" id="description">
-			<h2><?= get_field('description_title') ?></h2>
-			
-			<div>
-				<p>  <?= get_field('qualities')['0']['name'] ?> </p>
-				<img src="https://portfolio.localhost/wp-content/uploads/2023/06/Eclaires.png" alt="">
-				<?php
 
-				$image_url = 'https://portfolio.localhost/wp-content/uploads/2023/06/Eclaires.png'; // Remplacez l'URL par l'URL de votre image
+		<section class="quality" id="quality">
+			<h2 class="quality__title"><?= get_field('description_title') ?></h2>
 
-				$image_id = attachment_url_to_postid($image_url); // Obtient l'ID de l'image à partir de l'URL
+			<section class="quality__card">
+				<h3 class="card__title">  <?= get_field('qualities')['0']['name'] ?> </h3>
+				<?= image('https://portfolio.localhost/wp-content/uploads/2023/06/Eclaires.png', 'card__img', 'dessin de 3 éclaires'); ?>
+			</section>
 
-				if ($image_id) {
-					$image_size = 'project_thumbnail'; // Remplacez 'project_thumbnail' par la taille d'image souhaitée
-					$image = wp_get_attachment_image($image_id, $image_size, false, ['class' => 'description__img']);
-					echo $image; // Affiche l'image avec la taille spécifiée et la classe CSS 'animal__img'
-				} else {
-					// Aucun ID d'image n'a été trouvé pour l'URL donnée
-					// Gérez cette situation en conséquence
-				}
-				?>
+			<section class="quality__card">
+				<h3 class="card__title">  <?= get_field('qualities')['1']['name'] ?> </h3>
+				<?= image('https://portfolio.localhost/wp-content/uploads/2023/06/Soleil.png', 'card__img', 'dessin d\'un soleil '); ?>
+			</section>
 
-			</div>
-			
-			<div>
-				<p>  <?= get_field('qualities')['1']['name'] ?> </p>
-				<img src="https://portfolio.localhost/wp-content/uploads/2023/06/Soleil.png" alt="">
-			</div>
-			
-			<div>
-				<p>  <?= get_field('qualities')['2']['name'] ?> </p>
-				<img src="https://portfolio.localhost/wp-content/uploads/2023/06/ampoule.png" alt="">
-			</div>
-			
-			
-			<?php foreach (get_field('description') as $description): ?>
-				<p> <?= $description['paragraphe'] ?> </p>
-			<?php endforeach; ?>
-			
-			<?php foreach (get_field('legends_picture') as $legend): ?>
-				<img src="https://portfolio.localhost/wp-content/uploads/2023/06/2-bg-remove.png" alt="">
-				<p> <?= $legend['text']; ?> </p>
-			<?php endforeach; ?>
+			<section class="quality__card">
+				<h3 class="card__title">  <?= get_field('qualities')['2']['name'] ?> </h3>
+				<?= image('https://portfolio.localhost/wp-content/uploads/2023/06/ampoule.png', 'card__img', 'dessin d\'une ampoule'); ?>
+			</section>
+
+			<section class="description">
+				<h2 class="sr-only"> Mais encore </h2>
+
+				<figure class="description__fig">
+					<?= image('https://portfolio.localhost/wp-content/uploads/2023/06/2-bg-remove.png', 'description__img', 'photo de moi souriant'); ?>
+					<!--					<figcaption class="description__figcaption">-->
+					<?php //= get_field('legends_picture')[0]['text']
+					?><!--</figcaption>-->
+				</figure>
+
+				<div class="description__container">
+					<?php foreach (get_field('description') as $description): ?>
+						<p class="description__paragraph"> <?= $description['paragraphe'] ?> </p>
+					<?php endforeach; ?>
+				</div>
+			</section>
+
+
 		</section>
-		
-		<div class="project_container" id="project">
-			<?php
-			$call_to_action_text = get_field('call_to_action');
-			
-			// Faire une requête en DB pour récupérer 3 projets
-			$projects = new WP_Query([
-					'post_type' => 'projet',
-					'posts_per_page' => 9999
-			]); ?>
-			
-			<?php // Lancer la boucle pour afficher chaque projet
-			if ($projects->have_posts()): while ($projects->have_posts()):
-				$projects->the_post();
-				?>
-				<article>
-					<a href="<?= get_the_permalink(); ?>" class="project__link">
-						<span class=""><?= get_the_title(); ?></span>
-					</a>
-					<figure class="project__fig">
-						<?= get_the_post_thumbnail(null, 'project_thumbnail', ['class' => 'project__thumbnail']); ?>
-					</figure>
-				</article>
-			<?php endwhile; else: ?>
-				<p>Je n'ai aucun projet pour l'instant.</p>
-			<?php endif; ?>
+
+		<section class="projects" id="project">
+			<h2 class="projects__title"> <?= get_field('project_section_title') ?> </h2>
+			<div class="projects__container">
+				<?php
+				$call_to_action_text = get_field('call_to_action');
+
+				// Faire une requête en DB pour récupérer 3 projets
+				$projects = new WP_Query([
+						'post_type' => 'projet',
+						'posts_per_page' => 9999
+				]); ?>
+
+				<?php // Lancer la boucle pour afficher chaque projet
+				if ($projects->have_posts()): while ($projects->have_posts()):
+					$projects->the_post();
+					?>
+					<article class="project__card">
+						<a href="<?= get_the_permalink(); ?>" class="project__link">
+							<figure class="project__fig">
+								<?= get_the_post_thumbnail(null, 'project_thumbnail', ['class' => 'project__thumbnail']); ?>
+							</figure>
+							<h3 class="project__title"><?= get_the_title(); ?></h3>
+						</a>
+					</article>
+				<?php endwhile; else: ?>
+					<p>Je n'ai aucun projet pour l'instant.</p>
+				<?php endif;
+				wp_reset_query(); ?>
+			</div>
+		</section>
+		<div class="call-to-action">
+			<a href="https://portfolio.localhost/contact/"> <?= get_field('call_to_action') ?> </a>
 		</div>
-		<?php wp_reset_query(); ?>
-		<a href="https://portfolio.localhost/contact/">  <?= get_field('call_to_action') ?> </a>
 	</main>
-<?php endwhile; endif; ?>
+<?php endwhile;
+endif; ?>
 
 <?php get_footer(); ?>
