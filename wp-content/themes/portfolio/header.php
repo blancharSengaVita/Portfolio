1d728003f4c6ca837c430839a765ca0aaa9eb58f
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-06-22 08:01:24 +0200

    add favicon, update image url, add nav for desktop, put h2 for nav and document

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -6,18 +6,36 @@
 	<title><?= get_bloginfo('name'); ?></title>
 	<link rel="stylesheet" href="<?= get_stylesheet_directory_uri() . '/public/css/site.css';?>" />
 	<script src="<?= get_stylesheet_directory_uri() . '/public/js/main.js';?>" defer > </script>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
 </head>
 <body>
+<h1 class="sr-only"><?= get_bloginfo('name'); ?></h1>
 <header class="header">
 	<div class="header__container logo">
-		<a class="logo__link" href="https://portfolio.localhost/">
-			<?= image('https://portfolio.localhost/wp-content/uploads/2023/06/Logo.png', 'logo__img', 'Logo de Blanchar Senga-Vita') ?>
+		<a class="logo__link" href="<?= get_home_url()?> ">
+			<?= image(get_home_url().'/wp-content/uploads/2023/06/Logo.png', 'logo__img', 'Portfolio de Blanchar Senga-Vita') ?>
 		</a>
 	</div>
 
-	<div class="header__container menu">
-		<input type="checkbox" name="menu" id="menu" class="sr-only">
-		<label for="menu" class="menu__open">
+
+	<nav class="menu--desktop menu header__container display-none">
+		<h2 class="sr-only">Navigation principale</h2>
+			<?php foreach (portfolio_get_menu('main') as $link): ?>
+			<a href="<?= $link->href; ?>" class="nav__link">
+				<span class="nav__label">
+					<?= $link->label; ?>
+				</span>
+			</a>
+		<?php endforeach; ?>
+	</nav>
+
+	<div class="header__container menu menu--mobile ">
+		<h2 class="sr-only">Navigation principale</h2>
+			<input type="checkbox" name="menu" id="menu" class="sr-only menu__checkbox">
+		<label for="menu" class="menu__open ">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu" width="40" height="40" viewBox="0 0 24 24" stroke-width="1">
 				<line x1="4" x2="20" y1="12" y2="12"></line>
 				<line x1="4" x2="20" y1="6" y2="6"></line>
